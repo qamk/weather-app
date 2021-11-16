0d34cd45d20e36038a7ba346f5841504c7a28115
qamk <qamking@gmail.com>
2021-11-16 18:18:53 +0000

    Add webpack config with modules

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,24 @@ module.exports = {
 	entry: './src/index.js',
 	output: {
 		filename: 'bundle.js',
-		path: path.relative(__dirname, 'dist'),
+		path: path.resolve(__dirname, 'dist'),
 	},
+	devtool: "inline-source-map",
+	module: {
+		rules: [
+			{
+				test: /\.s[ac]ss/i,
+				use: [
+					'style-loader',
+					'css-loader',
+					'sass-loader',
+				],
+			},
+			{
+				test: /\.(jpeg|jpg|png|svg|gif)/i,
+				use: 'assets/resource'
+			}
+		],
+	}
 };
 
